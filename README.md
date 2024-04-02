de9bf9e15c7fe96c73b99741f79590ca5ed175d5
Paulina Brajovic <pollybrajovic@hotmail.com>
2024-04-02 01:03:18 -0300

    Subida inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="left"> Ejercicios Métodos de los arreglos </h1>
+
+<h4 align="left">
+:link: Url: <a href="https://paulabravic.github.io/mod3-js5-todo-list">https://paulabravic.github.io/mod3-js5-todo-list</a>
+</h4>
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+const listaDeTareas = document.querySelector("#tareas");
+const tareaInput = document.querySelector("#nuevaTarea");
+const btnAgregar = document.querySelector("#agregarTarea");
+const lblTotal = document.querySelector("#total");
+const lblRealizadas = document.querySelector("#realizadas");
+
+const tareas = [{
+    id: 1,
+    titulo: "Pasear al perro",
+    estado: false
+
+},
+{
+    id: 2,
+    titulo: "Hacer el Desafío",
+    estado: false
+
+},
+{
+    id: 3,
+    titulo: "Lavar la ropa",
+    estado: false
+},
+{
+    id: 4,
+    titulo: "Pagar las cuentas",
+    estado: false
+}
+];
+
+
+const fxMostrarListado = () => {
+
+    listaDeTareas.innerHTML = `<div class="titulo-listado flex-listado">
+                                <span>ID</span> 
+                                <span>Nombre</span>
+                                <span></span>
+                                <span></span>
+                               </div>`;
+
+    tareas.forEach(tarea => {
+
+        // Div registro
+        const div = document.createElement('div');
+        div.classList.add('flex-listado');
+        div.innerHTML = `<span>${tarea.id}</span> 
+                         <span>${tarea.titulo}</span>
+                        `;
+
+        //Checkbox
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.checked = tarea.estado;
+
+        checkbox.addEventListener('click', () => {
+            if (tarea.estado) {
+                tarea.estado = false;
+            } else {
+                tarea.estado = true;
+            }
+            fxMostrarListado();
+        });
+
+        // Imagen Eliminar
+        const img = document.createElement('img');
+        img.setAttribute('src', 'assets/img/eliminar.png');
+        img.setAttribute('alt', 'Eliminar');
+        img.classList.add('img-eliminar');
+
+        img.addEventListener('click', () => {
+            tareas.splice(tareas.indexOf(tarea), 1);
+            fxMostrarListado();
+        });
+
+        // Asociaciones
+        div.appendChild(checkbox);
+        div.appendChild(img);
+        listaDeTareas.appendChild(div);
+    });
+
+    lblTotal.innerHTML = tareas.length;
+    lblRealizadas.innerHTML = tareas.filter(x => x.estado === true).length;
+}
+
+
+btnAgregar.addEventListener("click", () => {
+    
+    const nuevaTarea = tareaInput.value;
+
+    if (nuevaTarea != "") {
+
+        const nuevoObj = {
+            id: tareas.length + 1,
+            titulo: nuevaTarea,
+            estado: false
+        }
+
+        tareas.push(nuevoObj);
+
+        tareaInput.value = "";
+
+        fxMostrarListado();
+    }
+});
+
+
+fxMostrarListado();
